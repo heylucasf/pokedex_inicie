cfa470c57021baef2ffe6a2aff20a189ca659801
Lucas Fabricio <lucas.fabricio1999@gmail.com>
2024-03-20 08:46:26 -0300

    fix: correção pokeId 0

--- spa/src/app/pages/detalhes/detalhes.component.ts
+++ spa/src/app/pages/detalhes/detalhes.component.ts
@@ -16,7 +16,7 @@ import { LoaderComponent } from "../../componentes/loader/loader.component";
 export class DetalhesComponent implements OnInit{
 
   pokemon: any[] = [];
-  idPoke!: number;
+  idPoke = 0;
   nomePoke = '';
   descPoke = '';
   ataquePoke = '';
